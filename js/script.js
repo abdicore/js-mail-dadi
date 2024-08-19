9e1aa1239b8e048d8d18cca55d51bc59961709b7
abdicore <abdirhamanmohamed@gmail.com>
2024-08-19 16:12:44 +0200

    risultato-in-pagina

--- js/script.js
+++ js/script.js
@@ -6,12 +6,15 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Stampiamo in pagina i due tiri e il risultato
 
 */
+//recupero elementi dal dom
+
+const divElement = document.getElementById('winner')
 
 // 1- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 let max = 10;
 let min = 1;
-let userNumber = Math.floor(Math.random() * max);
-console.log('user ' + userNumber);
+let userNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log('user ' + userNumber) ; document.getElementById('winner').innerHTML = userNumber;  
 
 let cpuNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 console.log('cpu ' + cpuNumber);
@@ -19,11 +22,17 @@ console.log('cpu ' + cpuNumber);
 // 2 Stabilire il vincitore, in base a chi fa il punteggio più alto.
  
 if (userNumber > cpuNumber) {
-    console.log('hai vinto');  
+    console.log('hai vinto');
+    //stampo in pagina
+    document.getElementById('winner').innerHTML = 'hai vinto';  
+
 } else {
     console.log('hai perso');
+    //stampo in pagina
+    document.getElementById('winner').innerHTML = 'hai perso';  
+
 }
 
 // 3 Stampiamo in pagina i due tiri e il risultato
 
-//recupero elementi dal dom
+
