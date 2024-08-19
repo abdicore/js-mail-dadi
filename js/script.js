d11dbddb466b0fa7d789ce0ca9d9496725a749c6
abdicore <abdirhamanmohamed@gmail.com>
2024-08-19 15:49:40 +0200

    stabilito-vincitore

--- js/script.js
+++ js/script.js
@@ -9,8 +9,21 @@ Stampiamo in pagina i due tiri e il risultato
 
 // 1- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 let max = 10;
+let min = 1;
 let userNumber = Math.floor(Math.random() * max);
 console.log('user ' + userNumber);
 
-let cpuNumber = Math.floor(Math.random() * max);
+let cpuNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 console.log('cpu ' + cpuNumber);
+
+// 2 Stabilire il vincitore, in base a chi fa il punteggio più alto.
+ 
+if (userNumber > cpuNumber) {
+    console.log('hai vinto');  
+} else {
+    console.log('hai perso');
+}
+
+// 3 Stampiamo in pagina i due tiri e il risultato
+
+//recupero elementi dal dom
